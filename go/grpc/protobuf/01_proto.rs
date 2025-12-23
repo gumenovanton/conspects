c00cc2ee804abf74d56b7a6a680e409bdbe98c86
gumenovanton <antongumenov@yandex.ru>
2025-12-23 15:41:21 +0500

    23.12.25

--- go/grpc/protobuf/01_proto.rs
+++ go/grpc/protobuf/01_proto.rs
@@ -1,47 +1,65 @@
-//>> ЗАДАЧА
+# PROTOBUF
+// контракт между клиентом и сервером
+// описывает какие методы может вызвать клиент и типы передаваемых параметров
+// один proto файл используется и на клиенте и на сервере
+
+# ЗАЧЕМ
 // для быстрого общения между сервисами
+// сука очень быстрый
 
-//>> УСТАНОВКА
-- go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
-- go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
-- export GO_PATH=~/go
-- export PATH=$PATH:/$GO_PATH/bin
-- go get google.golang.org/grpc
-
-//>> .PROTO файл
+# .PROTO ФАЙЛ
 //<< где
-// в корне в папке api создаем папку с названием сервиса
+// в корне в папке cmd создаем папку с названием сервиса
 // в ней создаем файл service.proto
 
-//<< начало
-// версия протокола
+# ПРИМЕР
 syntax = "proto3";
-// пакет
-package some_name;// нужно для импорта одного в другой
-// куда генерим код
-option go_package = "/pkg/gen";
 
-//<< как определить сообщение
-// в сообщении  нужно перечислить поля
-// указать тип, имя и порядковый номер (все переменные будут летать под номерами) 
-message Section {
-    string title = 1; // поля начинаются с единицы
-    string content = 2; 
+package user_v1; // для передачи одного в другой
+
+// этот параметр используется только в GO
+option go_package = "package name/pkg/user_v1/;user_v1" 
+// package name/pkg/user_v1 - хз что это, надо разобраться
+// user_v1 - такое имя получит пакет в генерируемом файле
+
+// это кастомный тип данных
+message UserInfo{
+    int64 id = 1;
+    string name = 2;
+    bool is_human = 3;
 }
 
-//<< основные типы
-// PROTO - GOLANG
-// int - int32
-// int64 - int64
-// uint32 - uint32
-// uint64 - uint64
-// float - float32
-// double - float64
-// bool - bool
-// string - string
-// bytes - []bytes
+//запрос 
+message GetRequest{
+    int64 id = 1;
+}
 
-//<< полем сообщения может быть перечисление
+// ответ
+message GetResponse{
+    UserInfo info = 1;
+}
+// это метод который будет дергать клиент
+service UserV1{
+    rpc Get(GetRequest) returns(GetResponse)
+}
+
+# ТИПЫ ДАННЫХ И ИХ АНАЛОГИ В GO
+//  PROTO       GO
+//  double      float32
+//  float       float64
+//  int32       int32
+//  int64       int64
+//  uint32      uint32
+//  uint64      uint64
+//  sint32      int32 - uptimized for negative numbers
+//  sint64      int32 - uptimized for negative numbers
+//  fixed32     fixed32 - uptimized for big numbers
+//  fixed64     fixed64 - uptimized for big numbers
+//  bool, string
+//  bytes       []byte - for files
+//  golang nulable type - all types can be this type, it means no value
+
+# ENUM
 // это набор строго определенных вариантов значений которые вообще можно передать в поле
 enum DayOfWeek {
     DAY_OF_WEEK_UNSPECIFIED = 0;// начинаются с 0 но в ноль всегда кладем заглушку
@@ -54,31 +72,31 @@ enum DayOfWeek {
     SUNDAY = 7;
 }
 
-//<< как определить сообщение с полем перечисления
+# СООБЩЕНИЕ С ENUM
 message Section {
     string title = 1; 
     string content = 2; 
     DayOfWeek type = 3; // здесь могут лежать только значения определенные в enum
 }
 
-//<< передача массивов и вложенные сообщения
+# МАССИВ С ВЛОЖЕННЫМ СООБЩЕНИЕМ
 // здесь я передаю массив сообщений Section
 // для обозначения массива ставлю слово repeated
 message TechnicalReview {
     repeated Section sections = 1; // Массив разделов обзора
 } 
 
-//<< могу передать мапу
+# МАПА
 message Environment {
     map<string, string> variables = 1;
 }
 
-//<< Могу указать поле как опционное
+# ОПЦИОНАЛЬНОЕ ПОЛЕ
 message Message {
     optional string bar = 1;//если поле не будет определено, в него будет помещено значение типа по умолчанию
 }
 
-//<< могу зарезервировать номера полей и имена
+# РЕЗЕРВИРОВАНИЕ ПОЛЕЙ ДЛЯ БУДУЩЕГО ИСПОЛЬЗОВАНИЯ
 // имена резервировать вообще нет смысла
 // но когда я думаю что в сообщении будет что то добавлено, я могу зарезервировать несколько полей
 // и если я меняю API и нужно удалить поле, нужно его зарезервировать
@@ -88,7 +106,7 @@ message Foo {
     reserved "foo", "bar";
 }
 
-//<< могу задавать сообщения как вложенные
+# ВЛОЖЕННЫЕ СООБЩЕНИЯ
 message SearchResponse {
     message Result {
       string url = 1;
@@ -98,19 +116,22 @@ message SearchResponse {
     repeated Result results = 1;
 }
 
-// переиспользование 
+# ПЕРЕИСПОЛЬЗОВАНИЕ 
 message SomeOtherMessage {
     SearchResponse.Result result = 1;
 }
 
-//>> КОДОГЕНЕРАЦИЯ
-protoc -I=api --go_out=. --go-grpc_out=. movie.proto 
-// -I=api - положить в папку api  
-// --go_out=. - флаг говорящий что надо генерировать protobuf и положить в gen(эта папка указана в файле .proto в строке - option go_package = "/gen";)
-// --go-grpc_out=. - флаг говорящий что надо генерировать gRPC и положить в gen(эта папка указана в файле .proto в строке - option go_package = "/gen";)
-// movie.proto - имя файла с которого генерируем код
-
-//>> ЛУЧШИЕ ПРАКТИКИ
-// - не меняй порядок полей, номера, имена, посто сделай новую версию    
-// - удаляя резервируй поля
-// - используй один файл на клиенте и на сервере, чтобы все было одинактво
+# ТИП УСТАНАВЛИВАЕМОГО СОЕДИНЕНИЯ
+service MyService {
+  // Унарный метод (запрос-ответ)
+  rpc UnaryMethod(Request) returns (Response) {};
+  
+  // Серверный поток
+  rpc ServerStreamMethod(Request) returns (stream Response) {};
+  
+  // Клиентский поток
+  rpc ClientStreamMethod(stream Request) returns (Response) {};
+  
+  // Двунаправленный поток
+  rpc BidirectionalStreamMethod(stream Request) returns (stream Response) {};
+}
